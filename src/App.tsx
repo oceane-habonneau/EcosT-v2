651523a1c7ab3e483cef3519118299399b8df37c
oceane-habonneau <oceane.habonneau@gmail.com>
2026-02-03 13:37:17 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { HotelEcosystem } from '@/app/components/HotelEcosystem';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100 p-8">
+      <HotelEcosystem />
+    </div>
+  );
+}
+
+export default App;
